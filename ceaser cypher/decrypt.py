b075a0b5f3e3ee10ab37bdb6c6057da2e4cd8f17
Nairitya Khilari <nkmann2@gmail.com>
2013-12-16 21:47:20 +0530

    modified

--- ceaser cypher/decrypt.py
+++ ceaser cypher/decrypt.py
@@ -1,15 +1,19 @@
 import sys
 
 enr_txt = raw_input('Encrypted text : ')
-size = len(enr_txt)
 q = raw_input('key was : ')
 q = int(q)
 dec_txt = ''
 
-for i in range(0,size):
-	asc = ord(enr_txt[i]) - q
-	if(asc<97):
-		dec_txt = dec_txt + chr(asc + 26)
-	else:
-		dec_txt = dec_txt + chr(ord(enr_txt[i]) - q)
-print dec_txt
+enr_txt = enr_txt.upper()
+Alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+for m in enr_txt:
+	if m in Alpha:
+		num = ord(m) - q
+		if(num < 65):
+			dec_txt = dec_txt + chr(num + 26)
+		else:
+			dec_txt = dec_txt + chr(num)
+
+print dec_txt.lower()
--- ceaser cypher/encrypt.py
+++ ceaser cypher/encrypt.py
@@ -1,21 +1,16 @@
 import sys
-import time
 st = raw_input('Enter the string : ')
 encrypted_text = ''
-string_size = len(st)
-q = raw_input('Enter the key (0-25) : ')
-Alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrrstuvwxyz'
-q = int(q)
-if(q>25 or q<0):
-	sys.stdout.write("Enter within limits\nExiting...\n")
-	time.sleep(1)
-	exit()
-i = 0
-for i in range(0,string_size):
-	asc = ord(st[i]) + q
-	if(asc > 122):
-		encrypted_text = encrypted_text + asc - 26
-	else:
-		encrypted_text = encrypted_text + chr(ord(st[i])+q)
-	i = i + 1
-print encrypted_text
+Alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+st = st.upper()
+key = raw_input('Encryption Key ??')
+key = int(key)
+for m in st:
+	if m in Alpha:
+		num = ord(m) + key
+		if(num > 90):
+			encrypted_text = encrypted_text + chr(num - 26)
+		else:
+			encrypted_text = encrypted_text + chr(num)
+
+print encrypted_text.lower()
