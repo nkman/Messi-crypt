9ec285cd2e0a8f9c38094b281547245bf93cb8e4
Nairitya Khilari <nkmann2@gmail.com>
2013-12-23 12:57:51 +0530

    Deleted certain files

--- Transposition cipher/sample.py
+++
@@ -1,8 +0,0 @@
-text = 'Forget the earth, Hez got the urge to pull his dick from the dirt and fuck the whole universe'
-key = 8
-import sys
-for i in range(0,len(text)):
-	sys.stdout.write(text[i] + ' ')
-	i = i + 1
-	if(i%key == 0):
-		print ''
